5decb3b01667514339845f5195a72be8c01f97cf
AS7AR <apo2073@outlook.com>
2026-06-10 10:57:03 +0900

    feat: 한글 검사 추가

--- crates/core/src/lib.rs
+++ crates/core/src/lib.rs
@@ -1,1 +1,12 @@
+mod assemble;
+pub(crate) mod constants;
+mod disassemble;
+mod err;
+mod hangul;
+mod utils;
 
+pub use assemble;
+pub use disassemble;
+pub use err;
+
+pub use crate::hangul::{is_hangul, is_hangul_alphabet, is_hangul_char, parser_hangul};
